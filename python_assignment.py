a2d2e7e03ea222037cab895c07869b1dd6e6ee5c
Geofrey <g.s.mushibi@students.uu.nl>
2019-01-15 15:52:11 +0100

    yearly and month average precipitation

--- python_assignment.py
+++ python_assignment.py
@@ -12,3 +12,21 @@ for measurement in precipitation:
        seattle_data[month - 1] += measurement['value'] #from the created list, we now add monthly data
 
 print(seattle_data)
+
+with open ('seattle_data.json ', 'w') as file: # open JSON file
+    json . dump ( seattle_data, file, indent =4, sort_keys = True ) #
+#write my_dictionary to file in a pretty JSON format
+
+####PART 2#####
+
+total_precipitation = 0
+for i in seattle_data:
+    total_precipitation += i
+
+print(total_precipitation)
+
+average = []
+for data in seattle_data:
+    average.append(data/total_precipitation)
+
+print(average)
